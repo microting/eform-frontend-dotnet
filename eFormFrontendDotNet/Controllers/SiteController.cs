dab96fad06eec228ba9dd0135483b104d0998e05
René Schultz Madsen <rm@microting.com>
2017-02-02 15:53:44 +0100

    Adding editing of a site

--- eFormFrontendDotNet/Controllers/SiteController.cs
+++ eFormFrontendDotNet/Controllers/SiteController.cs
@@ -30,5 +30,58 @@ namespace eFormFrontendDotNet.Controllers
 
             return View();
         }
+
+        public ActionResult Edit(int id)
+        {
+            string[] lines = System.IO.File.ReadAllLines(Server.MapPath("~/bin/Input.txt"));
+
+            string connectionStr = lines.First();
+
+            using (var db = new Models.Site(connectionStr))
+            {
+                try
+                {
+                    ViewBag.site = db.sites.Single(x => x.id == id);
+                    ViewBag.site_id = id;
+                    return View();
+                }
+                catch (Exception ex)
+                {
+                    System.IO.File.AppendAllText(Server.MapPath("~/bin/log/log.txt"), ex.ToString() + Environment.NewLine);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Update(int id)
+        {
+            string[] lines = System.IO.File.ReadAllLines(Server.MapPath("~/bin/Input.txt"));
+
+            string connectionStr = lines.First();
+
+            using (var db = new Models.Site(connectionStr))
+            {
+                try
+                {
+                    Models.sites site = db.sites.Single(x => x.id == id);
+                    var keys = Request.Form.AllKeys;
+                    int i = 0;
+                    foreach (string key in keys)
+                    {
+                        if (key.Contains("site['name']")) {
+                            site.name = Request.Form.Get(keys[i]);
+                        }
+                        i += 1;
+                    }
+                    db.SaveChanges();
+                    return Redirect("/Site");
+                }
+                catch (Exception ex)
+                {
+                    System.IO.File.AppendAllText(Server.MapPath("~/bin/log/log.txt"), ex.ToString() + Environment.NewLine);
+                }
+            }
+            return Redirect("/Site");
+        }
     }
 }
